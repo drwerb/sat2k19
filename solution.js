d5ebd43292478a654a4a16916b2f01cb8ac4072d
Vladimir Verblyudov <vverblyudov@amcbridge.com>
2019-02-28 20:55:11 +0200

    second

--- solution.js
+++ solution.js
@@ -1,20 +1,63 @@
 
 
+function Photo(conf, i) {
+    this.id = i;
+    this.pos = conf[0];
+    this.tags = conf.slice(2);
+    this.tagsSet = new Set(this.tags);
+    this.tagsLen = this.tags.length;
+    this.max = 0;
+    this.next = null;
+}
+
 var fs = require('fs');
 
 var stdinBuffer = fs.readFileSync(0); // STDIN_FILENO = 0
 var [ N, ...lines ] = stdinBuffer.toString().split('\r\n');
 
-//console.log(N + ".....");
-//console.log(lines);
-
 lines.pop();
 
-var p = lines.map((l) => l.split(''));
+var fotos = lines.map((l, idx) => new Photo(l.split(' '), idx));
 
-var sol = `3
-0 0 2 1
-0 2 2 2
-0 3 2 4`;
+var fotoSeq = [], fotoSeqSet = new Set();
 
-console.log(sol);
+fotoSeqSet.add(fotos[0]);
+
+fotos.forEach((f1) => {
+    if (fotoSeqSet.has(f1)) return;
+
+    fotos.forEach((f2) => {
+        let Sij = 0;
+
+        if (fotoSeqSet.has(f2)) return;
+
+        f1.tags.forEach((t1) => {
+            if (f2.tagsSet.has(t1)) {
+                Sij++;
+            }
+        });
+
+        let min = Math.min(f1.tagsLen - Sij, Sij, f1.tagsLen - Sij);
+
+        if (f1.max <= min) {
+            f1.max = min;
+            f1.next = f2;
+        }
+    });
+
+    fotoSeqSet.add(f1.next);
+});
+
+let f = fotos[0];
+
+while (true) {
+    console.log(f.id);
+    if (f.next == null) {
+        break;
+    }
+    else {
+        f = f.next;
+    }
+}
+
+//console.log(fotos);
